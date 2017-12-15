7520f8d3daa6ea00528de1e50ad5ce09923d18a2
andrew-atkinson <blind.sam@gmail.com>
2017-12-15 16:48:30 -0500

    updates: store with initial state and devTools

--- artist-profile/src/store.js
+++ artist-profile/src/store.js
@@ -1,6 +1,9 @@
 import {createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from './reducers'
 
-let store = createStore(rootReducer)
+export const initialState = {firstName: 'Your first name', lastName: 'last name', biography: 'biography here'}
+
+let store = createStore(rootReducer, initialState, composeWithDevTools())
 
 export default store
