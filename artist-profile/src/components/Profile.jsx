a736394a7a6b0e9973b2fef6ab16a184cab3d885
andrew-atkinson <blind.sam@gmail.com>
2017-12-15 19:49:47 -0500

    adds: flexbox spacers for center inputs

--- artist-profile/src/components/Profile.jsx
+++ artist-profile/src/components/Profile.jsx
@@ -4,10 +4,12 @@ import TextField from './TextField'
 export default class Profile extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
+        <div className='spacer'></div>
         <TextField name='firstName' dispatch={this.props.dispatchFirstName}/>
         <TextField name='lastName' dispatch={this.props.dispatchLastName}/>
         <TextField name='biography' dispatch={this.props.dispatchBiography}/>
+        <div className='spacer'></div>
       </div>
     )
   }
